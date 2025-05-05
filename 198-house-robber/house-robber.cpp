07731dee643f59676e08b58d4444c87c0bca8024
Saumya Pandey <pandeysaumya2004@gmail.com>
2025-05-05 20:56:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (22.24%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -43,17 +43,17 @@ class Solution {
 public:
     vector<int>dp;
 int fbu(vector<int>&arr){
-    int n=arr.size();
-    if(n==1) return arr[0];
-    dp.clear();
-    dp.resize(n);
-    // base case
-    dp[n-1]=arr[n-1];
-    dp[n-2]=max(arr[n-1],arr[n-2]);
-    for(int i=n-3;i>=0;i--){
-        dp[i]=max(arr[i]+dp[i+2],0+dp[i+1]);
-    }
-    return dp[0];
+   int n=arr.size();
+   if(n==1) return arr[0];
+   dp.clear();
+   dp.resize(n);
+//    base case
+dp[n-1]=arr[n-1];
+dp[n-2]=max(arr[n-1],arr[n-2]);
+for(int i=n-3;i>=0;i--){
+    dp[i]=max(arr[i]+dp[i+2],0+dp[i+1]);
+}
+return dp[0];
 }
 
 int rob(vector<int>&nums){
