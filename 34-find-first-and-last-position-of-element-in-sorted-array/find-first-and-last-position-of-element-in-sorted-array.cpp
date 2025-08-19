5c45ddf993bca9eff327825478dd1fbbe1117707
Saumya Pandey <pandeysaumya2004@gmail.com>
2025-08-20 01:09:43 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (52.47%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,112 @@
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//     vector<int>v(2);
+//     int n=nums.size();
+//     int lo=0;
+//     int hi=n-1;    
+//     v[0]=-1;
+//     v[1]=-1;
+//     //first occurance
+//     while(lo<=hi)
+//     {
+//         int mid=lo+(hi-lo)/2;
+//         if(nums[mid]==target){
+//             if(mid==0){
+//                 v[0]=mid;
+//                 break;
+//             }
+//             else if(nums[mid-1]!=target){
+//                 v[0]=mid;
+//                 break;
+//             }
+//             else {
+//                 hi=mid-1;
+//             }
+
+//         }
+//         else if(nums[mid]>target){
+//             hi=mid-1;
+//         }
+//         else{lo=mid+1;}
+//     }
+//     //last occurance
+//     lo=0;
+//     hi=n-1;
+    
+//     while(lo<=hi)
+//     {
+//         int mid=lo+(hi-lo)/2;
+//         if(nums[mid]==target){
+//             if(mid==n-1){
+//                 v[1]=mid;
+//                 break;
+//             }
+//             else if(nums[mid+1]!=target){
+//                 v[1]=mid;
+//                 break;
+//             }
+//             else {
+//                 lo=mid+1;
+//             }
+
+//         }
+//         else if(nums[mid]>target){
+//             hi=mid-1;
+//         }
+//         else if(nums[mid]<target)
+//         {lo=mid+1;}
+//     }
+//     return v;
+//     }
+// };
+
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+    int lo=0;
+    int hi=n-1;
+    vector<int>v(2);
+     v[0]=-1;
+     v[1]=-1;
+    while(lo<=hi){
+        int mid=lo+(hi-lo)/2;
+        if(nums[mid]== target){
+            if(mid==0) 
+            {v[0]=mid;
+            break;}
+            else if(nums[mid-1]!=target){
+                v[0]=mid;
+                break;
+            }
+            else {
+                hi=mid-1;
+            }
+        }
+        else if(nums[mid]>target) hi=mid-1;
+        else lo=mid+1;
+    }
+    lo=0;
+    hi=n-1;
+    while(lo<=hi){
+        int mid=lo+(hi-lo)/2;
+        if(nums[mid]==target){
+            if(mid==n-1){
+                v[1]=mid;
+                break;
+            }
+            else if(nums[mid+1]!=target){
+                v[1]=mid;
+                break;
+            }
+            else lo=mid+1;
+        }
+        else if(nums[mid]>target) hi=mid-1;
+
+        else lo= mid+1;
+    }
+    return v;
+    }
+};
