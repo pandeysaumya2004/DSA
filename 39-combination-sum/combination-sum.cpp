f8ad1352845eb574e673b4e179414c1ea3daff3f
Saumya Pandey <pandeysaumya2004@gmail.com>
2025-10-08 02:15:41 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (41.29%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -22,29 +22,55 @@
 // };
 
 
+// class Solution {
+// public:
+//    vector<vector<int>>result;
+//    void f(vector<int>&candidates,int t,int idx,vector<int>&subset){
+//     if(t==0){
+//         result.push_back(subset);
+//         return;
+//     }
+//     if(idx==candidates.size()) return;
+//     if(candidates[idx]<=t){
+//         subset.push_back(candidates[idx]);
+//           f(candidates,t-candidates[idx] ,idx,subset);
+//           subset.pop_back();
+//     }
+   
+//     f(candidates,t,idx+1,subset);
+    
+//    }
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         sort(candidates.begin(),candidates.end());
+//         vector<int>subset;
+//         result.clear();
+//         f(candidates,target,0,subset);
+//         return result;
+//     }
+// };
 class Solution {
 public:
-   vector<vector<int>>result;
-   void f(vector<int>&candidates,int t,int idx,vector<int>&subset){
+vector<vector<int>>result;
+void f(vector<int>&candidates,int t,int idx, vector<int>&subset){
     if(t==0){
         result.push_back(subset);
         return;
     }
-    if(idx==candidates.size()) return;
+    if(idx == candidates.size()) return;
     if(candidates[idx]<=t){
         subset.push_back(candidates[idx]);
-          f(candidates,t-candidates[idx] ,idx,subset);
-          subset.pop_back();
+        f(candidates,t-candidates[idx],idx,subset);
+        subset.pop_back();
+
     }
-   
     f(candidates,t,idx+1,subset);
-    
-   }
+}
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
         vector<int>subset;
         result.clear();
         f(candidates,target,0,subset);
         return result;
+      
     }
 };
