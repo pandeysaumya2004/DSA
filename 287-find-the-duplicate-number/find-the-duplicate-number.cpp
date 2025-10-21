d431b845d38f48eabba2e71493b97c39c34182a3
Saumya Pandey <pandeysaumya2004@gmail.com>
2025-10-21 21:54:59 +0530

    Time: 2 ms (72.83%) | Memory: 65.1 MB (42.54%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,29 @@
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         int n=nums.size();
+//         int i=0;
+//         while(i<n){
+//         int correctIdx=nums[i];
+//         if(nums[correctIdx]==nums[i]) return nums[i];
+//         else swap(nums[i],nums[correctIdx]);
+
+//     }
+//     return 100;
+//     }
+// };
+
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+     int n=nums.size();
+     int i=0;
+     while(i<n){
+        int correctIdx = nums[i];
+        if(nums[correctIdx]== nums[i]) return nums[i];
+        else swap(nums[i],nums[correctIdx]);
+     }
+     return 100;
+    }
+};
