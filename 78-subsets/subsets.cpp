02decbaa24be63ef2fc3f1ca3c74b0f26c132a22
Saumya Pandey <pandeysaumya2004@gmail.com>
2025-12-09 00:20:12 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (16.36%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,5 +1,5 @@
-// class Solution {
-// public:
+class Solution {
+public:
 // void helper(vector<int>ans,vector<vector<int>>&finalans,vector<int>&nums,int idx){
 //     if(idx==nums.size()){
 //         finalans.push_back(ans);
@@ -16,26 +16,21 @@
 //         helper(ans,finalans,nums,0);
 //         return finalans;
 //     }
-// };
-
-
-class Solution {
-public:
-void helper(vector<int>ans,vector<vector<int>>&finalans,vector<int>&num,int idx){
-    if(num.size() == idx){
+void helper(vector<int>ans,vector<vector<int>>&finalans,vector<int>&nums,int idx){
+    if(idx==nums.size()){
         finalans.push_back(ans);
         return;
     }
-    helper(ans,finalans,num,idx+1);
-    ans.push_back(num[idx]);
-    helper(ans,finalans,num,idx+1);
+    helper(ans,finalans,nums,idx+1);
+    ans.push_back(nums[idx]);
+    helper(ans,finalans,nums,idx+1);
 }
-
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>ans;
-        vector<vector<int>>finalans;
-        helper(ans,finalans,nums,0);
-        return finalans;
-
-        }
+vector<vector<int>> subsets(vector<int>& nums){
+    vector<int>ans;
+    vector<vector<int>>finalans;
+    helper(ans,finalans,nums,0);
+    return finalans;
+}
 };
+
+
