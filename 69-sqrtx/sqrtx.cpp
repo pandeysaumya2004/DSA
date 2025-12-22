cd9a95a242f42d9fe8f11fa1604d3cd41f0a3ce3
Saumya Pandey <pandeysaumya2004@gmail.com>
2025-12-22 20:06:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (50.36%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -11,16 +11,19 @@ public:
         //     else if(m*m>y) hi=mid-1;
         //     else lo=mid+1;
         // } return hi;
-        int lo=0;
-        int hi=x;
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            long long m=(long long)mid;
-            long long y=(long long)x;
-            if(m*m==y) return mid;
-            else if(m*m>y) hi=mid-1;
-            else lo=mid+1;
+       int lo=0;
+       int hi=x;
+       while(lo<=hi){
+        int mid=lo+(hi-lo)/2;
+        long long m= (long long)mid;
+        long long y = (long long )x;
+        if(m*m==y) return mid;
+        else if (m*m>y) hi=mid-1;
+        else lo=mid+1;
 
-        } return hi;
+        
+        }
+        return hi;
+
     }
 };
