2c332ab398deba4c898fd221c0f79cddb024fd02
Saumya Pandey <pandeysaumya2004@gmail.com>
2025-12-23 22:04:05 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (78.05%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -13,18 +13,34 @@ public:
         //     else{lo=mid+1;}
         // }
         // return 1;
+        // int n= arr.size();
+        // int lo=0;
+        // int hi=n-2;
+        // while(lo<=hi){
+        //     int mid = lo+(hi-lo)/2;
+        //     if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1])
+        //     return mid;
+        //     else if(arr[mid]>arr[mid+1]) hi=mid-1;
+        //     else lo=mid+1;
+
+
+        // }
+        // return 1;
         int n= arr.size();
-        int lo=0;
+        int lo=1;
         int hi=n-2;
         while(lo<=hi){
-            int mid = lo+(hi-lo)/2;
-            if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1])
-            return mid;
-            else if(arr[mid]>arr[mid+1]) hi=mid-1;
-            else lo=mid+1;
-
-
+            int mid=lo+(hi-lo)/2;
+            if(arr[mid]>arr[mid-1]&& arr[mid]>arr[mid+1]){
+                return mid;
+                break;
+            }
+            else if(arr[mid]>arr[mid-1]) lo=mid+1;
+            else hi=mid-1;
         }
-        return 1;
+        return -1;
+   
+
+
     }
 };
