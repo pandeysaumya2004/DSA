580346e3177700565aeb16e0580be3ec7d9847f5
Saumya Pandey <pandeysaumya2004@gmail.com>
2026-01-16 23:42:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (25.05%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -11,22 +11,24 @@
  */
 class Solution {
 public:
-void helper(TreeNode* root,string s,vector<string>&ans){
+void helper(TreeNode *root,string s,vector<string>&ans){
     if(root==NULL) return;
-    string a = to_string(root->val);
+    string a=to_string(root->val);
     if(root->left==NULL && root->right==NULL){
         s+=a;
         ans.push_back(s);
         return;
     }
     helper(root->left,s+a+"->",ans);
-    helper(root->right,s+a+"->",ans);
+     helper(root->right,s+a+"->",ans);
 
 }
+
 
     vector<string> binaryTreePaths(TreeNode* root) {
         vector<string>ans;
         helper(root,"",ans);
-        return ans;   
+        return ans;
+        
     }
 };
